b63fc912f54ba569a5ae3f3c9af61bf785f9be5a
조무현 <rio0930@jomuhyeons-MacBook-Pro.local>
2024-01-09 02:08:31 +0900

    Initial commit

---
+++ src/main/java/org/dragon/dragonmath/DragonMathApplication.java
@@ -0,0 +1,13 @@
+package org.dragon.dragonmath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DragonMathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DragonMathApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/dragon/dragonmath/DragonMathApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dragon.dragonmath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DragonMathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
